3d1e54a1cace2f0c4a303359cf26d724a2630cc6
Daniel Bender <1821759@stud.hs-mannheim.de>
2020-12-12 12:15:13 +0100

    Konto_testen implementiert (zum testen)

---
+++ Studienleistung2/Konto_testen.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+public class Konto_testen {
+
+    public static void main (String[] args){
+
+        HashMap<String,String> zugangsdaten = new HashMap<>();
+
+        zugangsdaten.put("DE1300123456","1234");
+        
+        Konto konto = new Konto("Daniel Bender","DE1300123456",zugangsdaten,1234.58,true);
+
+        System.out.println(konto.toString());
+        System.out.println(konto.getKontoinhaber());
+        System.out.println(konto.getKontonummer());
+        System.out.println(konto.getZugangsdaten());
+        System.out.println(konto.getKontostand());
+        System.out.println(konto.getAktiv());
+
+
+    }
+    
+}
