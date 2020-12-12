e1618ce8c6c691c22ce8d8993ccb4fc1c3ddc151
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2020-12-12 11:55:53 +0100

    Neuer Bank Konstruktor implementiert

--- Studienleistung2/Bank.java
+++ Studienleistung2/Bank.java
@@ -5,12 +5,19 @@ import java.util.List;
 class Bank{
     private List<Konto> kontoliste;
 
+    // Dieser Konstruktor bekommt Konten beim initialisieren übergeben
     public Bank(List<Konto> konten){
         this.kontoliste=konten;
+    }
+
+    // Dieser Konstruktor erstellt selbst neue Konten zum Testen
+    public Bank(){
+        this.kontoliste = Konto.createTenRandomAccounts();
     }
 
     private boolean validiereKonto(String kontonummer, String pin){
         return false;
     }
+
 
 }
