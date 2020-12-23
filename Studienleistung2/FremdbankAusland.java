b1c579e28423e570536cb5d4e9f9b90f6cddd01e
Daniel Bender <1821759@stud.hs-mannheim.de>
2020-12-23 12:44:07 +0100

    Bank - creatacc entfernt und in [...]Bank ausgel.

--- Studienleistung2/FremdbankAusland.java
+++ Studienleistung2/FremdbankAusland.java
@@ -6,9 +6,18 @@ public class FremdbankAusland extends Bank {
 
     FremdbankAusland(List<Konto> konten){
         super(konten);
+        createacc();
     }
 
     int berechneGebuehrinProzent(){
         return 0;
-    }    
+    }
+    
+    void createacc(){
+        super.kontoliste.add(new Konto("Juan Son","EN99991231","1000",120000.00,true));
+        super.kontoliste.add(new Konto("Wod Katitten","RU99999999","2000",1000400.00,true));
+        super.kontoliste.add(new Konto("Marie Joanna","NL04200420","0420",23.50,true));
+        super.kontoliste.add(new Konto("Vick Inger","NO58581235","0815",7852.00,true));
+        super.kontoliste.add(new Konto("Klaus Uhr","HE66606660","0666",1.00,false));
+    }
 }
