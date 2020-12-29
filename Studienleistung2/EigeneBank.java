5711bad4011dccb2371f1cc4c014abd1ddc44279
Annika Stark <1822999@stud.hs-mannheim.de>
2020-12-29 10:14:11 +0100

    änderungen

--- Studienleistung2/EigeneBank.java
+++ Studienleistung2/EigeneBank.java
@@ -1,8 +1,29 @@
 package Studienleistung2;
+
 public class EigeneBank extends Bank {
 
-    public int berechneGebuehrinProzent(){
+    public int berechneGebuehrinProzent() {
         return 0;
-    } {
-    
-}
+    }
+
+    {
+
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // TODO Auto-generated method stub
+        return super.equals(obj);
+    }
+
+    @Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
