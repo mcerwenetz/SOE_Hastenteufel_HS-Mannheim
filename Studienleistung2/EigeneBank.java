2abbaf22c79057ca5077f5c5d5f05f05a8a802d9
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2021-01-08 11:12:53 +0100

    Kunde hat jetzt Geld

--- Studienleistung2/EigeneBank.java
+++ Studienleistung2/EigeneBank.java
@@ -20,7 +20,7 @@ public class EigeneBank extends Bank {
         super.kontoliste.add(new Konto("Daniel Echt","DE00001232","1235",1000000.00,true));
         super.kontoliste.add(new Konto("Annika Stark","DE00001233","1236",6.50,true));
         super.kontoliste.add(new Konto("Daniel Bender","DE00001234","1237",5.00,true));
-        super.kontoliste.add(new Konto("Mark Hastenteufel","DE00001235","0000",0.00,false));
+        super.kontoliste.add(new Konto("Mark Hastenteufel","DE00001235","0000",1000.00,true));
     }
     
 }
