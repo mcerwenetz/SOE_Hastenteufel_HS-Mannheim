0d9aa6e86f3bc8915d0ee9d221fc17333b068e1d
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2021-01-08 11:13:14 +0100

    verschiedene Änderungen

--- Studienleistung2/Fremdbank.java
+++ Studienleistung2/Fremdbank.java
@@ -17,10 +17,12 @@ public class Fremdbank extends Bank {
     }
 
     void createacc(){
-        super.kontoliste.add(new Konto("Anne Wandnageln","DE11111231","0001",12345.00,true));
-        super.kontoliste.add(new Konto("Willfried Lichwichsen","DE22221232","0002",54321.00,true));
-        super.kontoliste.add(new Konto("Willma Besamen","DE44441233","0003",78945.00,true));
+        super.kontoliste.add(new Konto("Anna Theke","DE11111231","0001",12345.00,true));
+        super.kontoliste.add(new Konto("Axel Höhle","DE22221232","0002",54321.00,true));
+        super.kontoliste.add(new Konto("Rainer Zufall","DE44441233","0003",78945.00,true));
+        super.kontoliste.add(new Konto("Ernst Fall","DE44441228","0003",78945.00,true));
         super.kontoliste.add(new Konto("Karl Laschnikow","DE55551234","0004",98745.00,true));
+        //gesperrtes Konto
         super.kontoliste.add(new Konto("Peter Silie","DE66661235","0005",6547.00,false));
     }
     
--- Studienleistung2/FremdbankAusland.java
+++ Studienleistung2/FremdbankAusland.java
@@ -17,9 +17,10 @@ public class FremdbankAusland extends Bank {
     
     void createacc(){
         super.kontoliste.add(new Konto("Juan Son","EN99991231","1000",120000.00,true));
-        super.kontoliste.add(new Konto("Wod Katitten","RU99999999","2000",1000400.00,true));
+        super.kontoliste.add(new Konto("Joe Ker","RU99999999","2000",1000400.00,true));
         super.kontoliste.add(new Konto("Marie Joanna","NL04200420","0420",23.50,true));
         super.kontoliste.add(new Konto("Vick Inger","NO58581235","0815",7852.00,true));
+        //gesperrt
         super.kontoliste.add(new Konto("Klaus Uhr","HE66606660","0666",1.00,false));
     }
 }
